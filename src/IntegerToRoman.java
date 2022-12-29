76c1049ae5857681553c2f1d49b7f9ba42755f79
rick <>
2022-12-30 05:34:43 +0900

    working on Integer to Roman

---
+++ src/IntegerToRoman.java
@@ -0,0 +1,21 @@
+public class IntegerToRoman {
+    public static void main(String[] args) {
+      String str =   intToRoman(3);
+        System.out.println(str);
+
+    }
+
+
+    static String intToRoman(int num) {
+        String text = String.valueOf(num);
+        int length = text.length();
+        int position = 0;
+        for (int i = 0 ; i < length; i++){
+
+        }
+
+
+
+
+    }
+}
